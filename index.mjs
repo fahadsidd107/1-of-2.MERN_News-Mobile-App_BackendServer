76b857bbd344d3e20b060b91deaa2a355895605f
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-05 13:43:31 +0500

    work

--- index.mjs
+++ index.mjs
@@ -2,7 +2,9 @@ import express from 'express'
 const app= express()
 const PORT = 7777
 
-app.get('/', (req, res) => {})
+app.get('/', (req, res) => {
+    res.send()
+})
 
 app.listen(PORT,()=>{
     console.log(`Your app is running at ${PORT}`)
