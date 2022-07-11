790c6885b6eafd3f51d6a67a611e48ce67c8fcde
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-12 02:46:08 +0500

    work

--- news/news.mjs
+++ news/news.mjs
@@ -3,6 +3,7 @@ import fs from 'fs'
 class News{
     constructor(filename){
         this.filename = filename
-        fs.accessSync(this.filename, fs.constants.R_OK)
+        
+        fs.accessSync(this.filename)
     }
 }
