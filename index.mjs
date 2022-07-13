cb8b1611440c4e7f6516b7625b2410af1bf0e609
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-14 02:37:15 +0500

    work complete

--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
 import express from 'express'
-// import News from './news/news.mjs'
+import News from './news/news.mjs'
 const app= express()
 const PORT = 7777
 
 app.use(express.static('public'))
 
-// new News('test.txt')
+new News('test.txt')
 
 // app.get('/', (req, res) => {
 //     res.send(`<h1>Hello Server</h1>`)
