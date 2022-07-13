7f3c4fbae421e803c9dccc89eb43a062d68b225a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-14 02:36:16 +0500

    work

--- news/news.mjs
+++ news/news.mjs
@@ -1,23 +1,21 @@
-import fs from 'fs'
+import fs from "fs";
 
- class News{
-    constructor(filename = 'news.json'){
-        this.path = `./data/${filename}`
+class News {
+  constructor(filename = "news.json") {
+    this.path = `./data/${filename}`;
 
-        try{
-fs.readdirSync('data')
-        }catch(err){
-fs.mkdirSync('data')
-        }
-        
-        try{
-            fs.accessSync(this.path)
-        }catch(err){
-            fs.writeFileSync(this.path, '[]')
-        }
-       
-            
+    try {
+      fs.readdirSync("data");
+    } catch (err) {
+      fs.mkdirSync("data");
     }
+
+    try {
+      fs.accessSync(this.path);
+    } catch (err) {
+      fs.writeFileSync(this.path, "[]");
+    }
+  }
 }
 
-export default News
+export default News;
