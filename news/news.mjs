5113a8a53a39547c1d47b254f66e64e1acf76099
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-18 17:31:32 +0500

    work

--- news/news.mjs
+++ news/news.mjs
@@ -25,7 +25,7 @@ class News {
 const totalData = JSON.parse(await fs.promises.readFile(this.path, "utf8"))
 const id = this.createId()
 totalData.push(data)
-// console.log(totalData)
+console.log(id)
 
 await fs.promises.writeFile(this.path, JSON.stringify(totalData, null, 2))
   }
