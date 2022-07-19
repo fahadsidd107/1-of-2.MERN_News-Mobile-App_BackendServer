56d70bf24449b64e754028a1ca8258fc7cc9f3c3
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-19 19:56:12 +0500

    work

--- news/news.mjs
+++ news/news.mjs
@@ -18,20 +18,20 @@ class News {
   }
 
   createId() {
-   return new Date().getTime().toString();
+    return new Date().getTime().toString();
   }
 
   async create(data) {
     const totalData = await this.getAll();
     const id = this.createId();
-    totalData.push({...data, id});
+    totalData.push({ ...data, id });
     // console.log(id);
 
     await fs.promises.writeFile(this.path, JSON.stringify(totalData, null, 2));
   }
 
- async getAll() {
-   return JSON.parse(await fs.promises.readFile(this.path, "utf8"))
+  async getAll() {
+    return JSON.parse(await fs.promises.readFile(this.path, "utf8"));
   }
 
   async getOne(id) {
