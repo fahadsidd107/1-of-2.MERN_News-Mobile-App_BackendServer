a4b1dab7972749133f772dd888f7eb6c477eb21a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-21 18:34:39 +0500

    work

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ const PORT = 7777
 app.use(express.static('public'))
 
 const news = new News()
-news.create({title:"test",content:"test",thumbnail:"test",category:"test"})
+news.create({title:"test",content:"test",thumbnail:"test",category:"tech"})
 
 // app.get('/', (req, res) => {
 //     res.send(`<h1>Hello Server</h1>`)
