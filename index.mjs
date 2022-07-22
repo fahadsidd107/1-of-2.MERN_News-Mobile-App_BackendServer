ee661e740408eb6be32e9294831967df5cc05cdc
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-23 01:27:26 +0500

    work

--- index.mjs
+++ index.mjs
@@ -1,19 +1,19 @@
-import express from 'express'
-import News from './news/news.mjs'
-const app= express()
-const PORT = 7777
+import express from "express";
+import News from "./news/news.mjs";
+const app = express();
+const PORT = 7777;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const news = new News()
-const data = news.getAll()
-console.log(data)
+const news = new News();
+const data = news.getAll();
+console.log(data);
 
 const test = async () => {
-    const data = await news.getByCategory('tech')
-    console.log(data)
-}
-test()
+  const data = await news.getByCategory("tech");
+  console.log(data);
+};
+test();
 
 // news.create({title:"test",content:"test",thumbnail:"test",category:"tech"})
 
@@ -21,6 +21,6 @@ test()
 //     res.send(`<h1>Hello Server</h1>`)
 // })
 
-app.listen(PORT,()=>{
-    console.log(`Your app is running at ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Your app is running at ${PORT}`);
+});
