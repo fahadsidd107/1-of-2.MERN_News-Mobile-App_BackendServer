e77d1eedefa63b4df23fcd9ec91d55cda3c1761b
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-24 05:10:56 +0500

    work

--- routes/news.mjs
+++ routes/news.mjs
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router()
 
-router.post('/create', (req, res) => {})
+router.post('/create', (req, res) => {
+    res.send(`<h1>Hello Server</h1>`)
+})
 
 module.exports = router;
