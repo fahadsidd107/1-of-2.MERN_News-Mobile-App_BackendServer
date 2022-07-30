b9d7ead178b3a94d13ca56a8169123fbc10fcf54
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-30 20:36:51 +0500

    work

--- routes/news.mjs
+++ routes/news.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 const multer = require("multer");
+multer.memoryStorage()
 
 router.post("/create", (req, res) => {
   res.send("Hello Server");
