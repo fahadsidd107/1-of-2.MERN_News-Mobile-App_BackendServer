e37146dd7bdebbda24a2d37c401ba07008903f90
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-31 14:02:55 +0500

    work

--- routes/news.mjs
+++ routes/news.mjs
@@ -1,12 +1,12 @@
 import express from "express";
 const router = express.Router();
-import multer from 'multer'
-const storage = multer.memoryStorage()
-const uploads = multer({storage})
+import multer from "multer";
+const storage = multer.memoryStorage();
+const uploads = multer({ storage });
 
-router.post("/create",uploads.single('thumbnail'), (req, res) => {
-  console.log('file',req.file);
-  console.log('body',req.body);
+router.post("/create", uploads.single("thumbnail"), (req, res) => {
+  console.log("file", req.file);
+  console.log("body", req.body);
   res.send("Hello Server");
 });
 
