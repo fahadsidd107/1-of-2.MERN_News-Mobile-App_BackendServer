ef82a6eeb53208cd93734da21b47934da640a4b1
fahadsidd107 <fsiddiqui107@gmail.com>
2022-08-01 18:57:26 +0500

    work

--- routes/news.js
+++ routes/news.js
@@ -4,10 +4,10 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const uploads = multer({ storage });
 
-router.post("/create", uploads.single("thumbnail"), (req, res) => {
-  console.log(req.file);
-  console.log( req.body);
-  res.send("Hello Server");
+router.post("/create", uploads.single("thumbnail"), async (req, res) => {
+ await console.log(req.file);
+await  console.log( req.body);
+ await res.send("Hello Server");
 });
 
 module.exports = router;
