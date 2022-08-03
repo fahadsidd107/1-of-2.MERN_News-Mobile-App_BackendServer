a71c57630c79a6da0333380107cfc4bbe8224e97
fahadsidd107 <fsiddiqui107@gmail.com>
2022-08-04 00:07:14 +0500

    work

--- routes/news.js
+++ routes/news.js
@@ -3,11 +3,23 @@ const router = express.Router();
 const multer = require("multer");
 const sharp = require("sharp");
 const storage = multer.memoryStorage();
+const fs = require("fs");
 const uploads = multer({ storage });
+const News = require("../news/news");
 
 router.post("/create", uploads.single("thumbnail"), async (req, res) => {
- console.log(req.file);
-  console.log( req.body);
+  console.log(req.file);
+  console.log(req.body);
+  fs.access("./data/uploads", (err) => {
+    if (err) {
+      fs.mkdirSync("./data/uploads");
+    }
+  }
+  );
+  const id = new News().createId();
+  const formattedName = req.file.originalname.split(" ").join("-");
+  const filename = `${id}-${formattedName}`;
+  await sharp(req.file.buffer).resize({ width: 615, height: 350 }).toFile('./data/uploads' + filename);
   res.send("Hello Server");
 });
 
